394a6728255e18cac5c7329ba97f7b85f48154e0
Vitaly Shlyaga <storkvist@storkvist.net>
2016-05-06 00:40:19 +0300

    Select lessons only from students branch.

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -2,6 +2,10 @@ class LessonsController < ApplicationController
   load_and_authorize_resource
 
   def index
+    if student_signed_in?
+      @lessons = @lessons.where(branch_id: current_student.branch_id)
+    end
+
     respond_to do |format|
       format.json
     end
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -9,7 +9,7 @@ class Lesson < ActiveRecord::Base
   has_many :lessons_students, class_name: 'LessonStudent', dependent: :destroy
   has_many :students, through: :lessons_students
 
-  validates_presence_of :course, :start_time, :end_time
+  validates_presence_of :course, :branch, :start_time, :end_time
 
   def time_interval
     "#{start_time.strftime('%H:%M')}–#{end_time.strftime('%H:%M')}"
