e81fb3fc7195fe8041580f5af1af33db00e6a644
Vitaly Shlyaga <storkvist@storkvist.net>
2016-05-05 12:22:08 +0300

    Email for course enrollment confirmation.

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,7 +2,7 @@ class ApplicationMailer < ActionMailer::Base
   default from: 'no-reply@aidah-manaar.com'
   layout 'mailer'
 
-  def confirm_course_emrollment(student, course)
+  def confirm_course_enrollment(student, course)
     mail(to: student.email, subject: 'Confirmation of course enrollment')
   end
 end
