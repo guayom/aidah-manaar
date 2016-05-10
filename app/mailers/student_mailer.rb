09745744f820e5fbb3b77410d8af7b0a2cb2e375
Vitaly Shlyaga <storkvist@storkvist.net>
2016-05-10 13:39:03 +0300

    @student variables in StudentMailer.

--- app/mailers/student_mailer.rb
+++ app/mailers/student_mailer.rb
@@ -1,9 +1,11 @@
 class StudentMailer < ApplicationMailer
   def confirm_registration(student)
+    @student = student
     mail(to: student.email, subject: t('devise.registrations.thank_you.title'))
   end
 
   def confirm_course_enrollment(student, course)
+    @student = student
     @course = course
     mail(to: student.email,
          subject: t('helpers.student.you_are_enrolled_to_course', course_name: course.name))
