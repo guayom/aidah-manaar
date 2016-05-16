222ec5493dc441ebfd32e0b17a323560c5199c35
Vitaly Shlyaga <storkvist@storkvist.net>
2016-05-16 21:22:58 +0300

    Added thumbnail for homepage carousel.

--- app/models/slide.rb
+++ app/models/slide.rb
@@ -1,6 +1,5 @@
 class Slide < ActiveRecord::Base
-  has_attached_file :image,
-                    styles: { medium: '300x300>', thumb: '100x100>' },
+  has_attached_file :image, styles: { homepage: '2880x1800>' },
                     default_url: '/images/:style/missing.png'
   validates_attachment :image, content_type: { content_type: /\Aimage\/.*\Z/ }, presence: true
 
