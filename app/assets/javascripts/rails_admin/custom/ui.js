97d803ef75f976324e170de8b304ccc68a1f2141
Vitaly Shlyaga <storkvist@storkvist.net>
2016-05-25 23:15:40 +0300

    -bootsy +ckeditor

--- app/assets/javascripts/rails_admin/custom/ui.js
+++ app/assets/javascripts/rails_admin/custom/ui.js
@@ -1,15 +1,13 @@
-//= require bootsy
 //= require video
 
 $(function() {
-  $(document).on('pjax:complete', Bootsy.init);
-
   $(document).on('pjax:start', function() {
     $('.video-js').each(function() {
       var video = videojs(this);
       video.dispose();
     });
   });
+
   $(document).on('pjax:end', function() {
     $('.video-js').each(function() {
       videojs(this);
