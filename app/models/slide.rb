bfe57466d961740026c03c65f0490fbe4258f933
Vitaly Shlyaga <storkvist@storkvist.net>
2016-06-28 23:14:18 +0300

    Debug S3 config.

--- app/models/slide.rb
+++ app/models/slide.rb
@@ -1,6 +1,6 @@
 class Slide < ActiveRecord::Base
-  has_attached_file :image, styles: { homepage: '1440x900>' },
-                    default_url: '/images/:style/missing.png'
+  has_attached_file :image, styles: { homepage: '1440x900>' }#,
+                    # default_url: '/images/:style/missing.png'
   validates_attachment :image, content_type: { content_type: /\Aimage\/.*\Z/ }, presence: true
 
   rails_admin do
