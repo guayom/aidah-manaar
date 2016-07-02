603d4850e1fb83af4a0ff21498bd2fec4cac120f
Vitaly Shlyaga <storkvist@storkvist.net>
2016-07-02 23:38:20 +0300

    Test code for import.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,9 +4,18 @@ class HomeController < ApplicationController
 
   def import
     JSON.parse(
+      Net::HTTP.get(URI('http://aidah-manaar.herokuapp.com/export/branches.json'))
+    ).each do |data|
+      if !Branch.find_by(id: data['id'])
+        b = Branch.new(data)
+        b.save(validate: false)
+      end
+    end
+
+    JSON.parse(
       Net::HTTP.get(URI('http://aidah-manaar.herokuapp.com/export/students.json'))
     ).each do |data|
-      if !Student.find_by(id: data['id'])
+      if !Student.find_by(id: data['id']) && !Student.find_by(email: data['email'])
         s = Student.new(data)
         s.save(validate: false)
       end
