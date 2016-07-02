b11128bb0e0cccc2b90a2f7099cb56fececd0b40
Vitaly Shlyaga <storkvist@storkvist.net>
2016-07-02 23:26:58 +0300

    Test code for import.

--- app/models/student.rb
+++ app/models/student.rb
@@ -27,4 +27,35 @@ class Student < ActiveRecord::Base
   def address
     "#{district.canton.province}, #{district.canton}, #{district}"
   end
+
+  def as_json(options = nil)
+    attrs = {
+      only: [
+        'id',
+        'email',
+        'encrypted_password',
+        'reset_password_token',
+        'reset_password_sent_at',
+        'remember_created_at',
+        'sign_in_count',
+        'current_sign_in_at',
+        'last_sign_in_at',
+        'current_sign_in_ip',
+        'last_sign_in_ip',
+        'created_at',
+        'updated_at',
+        'first_name',
+        'last_name',
+        'phone',
+        'district_id',
+        'branch_id',
+        'id_number',
+        'beginner',
+        'birthdate',
+        'second_last_name'
+      ]
+    }
+
+    super(attrs.merge(options || {}))
+  end
 end
