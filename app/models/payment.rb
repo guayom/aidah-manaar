f4a611053326a07934809504d24cf89d726a8acd
Vitaly Shlyaga <storkvist@storkvist.net>
2016-07-03 01:38:13 +0300

    Mark invoices as payed if sum from its payments is enough.

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,4 +1,12 @@
 class Payment < ActiveRecord::Base
   belongs_to :student
   belongs_to :invoice
+
+  after_create do
+    total_sum = invoice.items.map(&:price).sum
+    payed_sum = invoice.payments.map(&:sum).sum
+    if payed_sum >= total_sum
+      invoice.update!(payed: true)
+    end
+  end
 end
