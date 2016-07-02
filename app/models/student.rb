b6fb77f3bab419393e9c83f6825653e6fd3bef8e
Vitaly Shlyaga <storkvist@storkvist.net>
2016-07-02 23:34:10 +0300

    Test code for import.

--- app/models/student.rb
+++ app/models/student.rb
@@ -40,8 +40,6 @@ class Student < ActiveRecord::Base
         'sign_in_count',
         'current_sign_in_at',
         'last_sign_in_at',
-        'current_sign_in_ip',
-        'last_sign_in_ip',
         'created_at',
         'updated_at',
         'first_name',
