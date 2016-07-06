c89608280957aae7a06cba9705cf8004f2319d05
Vitaly Shlyaga <storkvist@storkvist.net>
2016-07-06 15:46:34 +0300

    show list of payments for invoice

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,4 +1,5 @@
 class Invoice < ActiveRecord::Base
+
   belongs_to :student
 
   has_many :items, class_name: 'InvoiceItem', inverse_of: :invoice
@@ -14,5 +15,25 @@ class Invoice < ActiveRecord::Base
     SendInvoiceJob.perform_later(self)
 
     update!(sent: true)
+  end
+
+  # RAILS_ADMIN_CONFIG
+
+  rails_admin do
+    list do
+      field :id
+      field :payed
+      field :payments_for_invoice do
+        pretty_value do
+          path = bindings[:view].index_path(
+            model_name: 'payment',
+            f: { invoice: { rand(1000000) => { o: 'is', v: bindings[:object].id } } }
+          )
+          "<a href=#{path}>Payments</a>".html_safe
+        end
+      end
+      field :student
+      field :created_at
+    end
   end
 end
