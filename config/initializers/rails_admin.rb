ac533ac3ee8d81ee2ea77eb63a346f4cefe7e661
Vitaly Shlyaga <storkvist@storkvist.net>
2016-07-21 16:14:21 +0300

    remove old :bootsy config

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -205,8 +205,8 @@ RailsAdmin.config do |config|
     edit do
       exclude_fields :children, :self_and_ancestors, :self_and_descendants
 
-      field :full_description, :bootsy
-      field :public_description, :bootsy
+      field :full_description, :ck_editor
+      field :public_description, :ck_editor
     end
   end
 
