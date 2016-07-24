37bfbcddd16c4baeb65d5b7eb8dbee23dafafddc
Vitaly Shlyaga <storkvist@storkvist.net>
2016-07-24 20:38:11 +0300

    notify main admins about new partial invoice

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -4,4 +4,10 @@ class AdminMailer < ApplicationMailer
     mail(to: Admin.where(main: true).pluck(:email),
          subject: t('helpers.admin.new_student_added'))
   end
+
+  def notify_about_partial_invoice(invoice)
+    @invoice = invoice
+    mail(to: Admin.where(main: true).pluck(:email),
+         subject: t('helpers.admin.new_partial_invoice_added'))
+  end
 end
