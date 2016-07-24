6c652b67ea63dde92800d91431173a94d8275ed9
Vitaly Shlyaga <storkvist@storkvist.net>
2016-07-24 22:14:58 +0300

    add initial check for first ever task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ActiveRecord::Base
   def self.need?(name)
-    Task.where(name: name).last.created_at.to_date.beginning_of_month < Date.today.beginning_of_month
+    last_task = Task.where(name: name).last
+    last_task.blank? || (last_task.created_at.to_date.beginning_of_month < Date.today.beginning_of_month)
   end
 
   def self.finish!(name)
