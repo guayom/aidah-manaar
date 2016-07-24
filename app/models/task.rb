0db6094fa946cd0dbd58559a7ee2a4d3e7c8eaad
Vitaly Shlyaga <storkvist@storkvist.net>
2016-07-24 22:11:49 +0300

    switch Task methods to class scope

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,9 +1,9 @@
 class Task < ActiveRecord::Base
-  def need?(name)
+  def self.need?(name)
     Task.where(name: name).last.created_at.to_date.beginning_of_month < Date.today.beginning_of_month
   end
 
-  def finish!(name)
+  def self.finish!(name)
     Task.create!(name)
   end
 end
