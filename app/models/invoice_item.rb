34f9ecd31949dfd9e6e37a39cdf31b00f5bc14ee
Vitaly Shlyaga <storkvist@storkvist.net>
2016-08-01 18:04:17 +0300

    InvoiceItem price should be not null by default

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,6 +1,8 @@
 class InvoiceItem < ActiveRecord::Base
   belongs_to :invoice, inverse_of: :items
 
+  validates_numericality_of :price
+
   def has_discount?
     price_with_discount.present?
   end
