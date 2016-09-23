37fad1c7113ab3dfb317ffabf99d93cb24fd6db0
Vitaly Shlyaga <storkvist@storkvist.net>
2016-09-23 18:05:58 +0300

    import should only save new students and do not touch current ones

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,20 +7,17 @@ class HomeController < ApplicationController
   end
 
   def import
-    CourseStudent.destroy_all
-    LessonStudent.destroy_all
-
-    JSON.parse(
-      Net::HTTP.get(URI('http://aidah-manaar.herokuapp.com/export/branches.json'))
-    ).each do |data|
-      bb = Branch.find_by(id: data['id'])
-      if !bb
-        b = Branch.new(data)
-        b.save!(validate: false)
-      else
-        bb.update!(data)
-      end
-    end
+    # JSON.parse(
+    #   Net::HTTP.get(URI('http://aidah-manaar.herokuapp.com/export/branches.json'))
+    # ).each do |data|
+    #   bb = Branch.find_by(id: data['id'])
+    #   if !bb
+    #     b = Branch.new(data)
+    #     b.save!(validate: false)
+    #   else
+    #     bb.update!(data)
+    #   end
+    # end
 
     JSON.parse(
       Net::HTTP.get(URI('http://aidah-manaar.herokuapp.com/export/students.json'))
@@ -30,68 +27,68 @@ class HomeController < ApplicationController
         s = Student.new(data)
         s.save!(validate: false)
       else
-        ss.update!(data)
+        # ss.update!(data)
       end
     end
 
-    JSON.parse(
-      Net::HTTP.get(URI('http://aidah-manaar.herokuapp.com/export/courses.json'))
-    ).each do |data|
-      cc = Course.find_by(id: data['id'])
-      if !cc
-        c = Course.new(data)
-        c.save!(validate: false)
-      else
-        cc.update!(data)
-      end
-    end
+    # JSON.parse(
+    #   Net::HTTP.get(URI('http://aidah-manaar.herokuapp.com/export/courses.json'))
+    # ).each do |data|
+    #   cc = Course.find_by(id: data['id'])
+    #   if !cc
+    #     c = Course.new(data)
+    #     c.save!(validate: false)
+    #   else
+    #     cc.update!(data)
+    #   end
+    # end
 
-    JSON.parse(
-      Net::HTTP.get(URI('http://aidah-manaar.herokuapp.com/export/course_students.json'))
-    ).each do |data|
-      cscs = CourseStudent.find_by(id: data['id'])
-      if !cscs
-        cs = CourseStudent.new(data)
-        cs.save!(validate: false)
-      else
-        cscs.update!(data)
-      end
-    end
+    # JSON.parse(
+    #   Net::HTTP.get(URI('http://aidah-manaar.herokuapp.com/export/course_students.json'))
+    # ).each do |data|
+    #   cscs = CourseStudent.find_by(id: data['id'])
+    #   if !cscs
+    #     cs = CourseStudent.new(data)
+    #     cs.save!(validate: false)
+    #   else
+    #     cscs.update!(data)
+    #   end
+    # end
 
-    JSON.parse(
-      Net::HTTP.get(URI('http://aidah-manaar.herokuapp.com/export/instructors.json'))
-    ).each do |data|
-      ii = Instructor.find_by(id: data['id'])
-      if !ii
-        i = Instructor.new(data)
-        i.save!(validate: false)
-      else
-        ii.update!(data)
-      end
-    end
+    # JSON.parse(
+    #   Net::HTTP.get(URI('http://aidah-manaar.herokuapp.com/export/instructors.json'))
+    # ).each do |data|
+    #   ii = Instructor.find_by(id: data['id'])
+    #   if !ii
+    #     i = Instructor.new(data)
+    #     i.save!(validate: false)
+    #   else
+    #     ii.update!(data)
+    #   end
+    # end
 
-    JSON.parse(
-      Net::HTTP.get(URI('http://aidah-manaar.herokuapp.com/export/lessons.json'))
-    ).each do |data|
-      ll = Lesson.find_by(id: data['id'])
-      if !ll
-        l = Lesson.new(data)
-        l.save!(validate: false)
-      else
-        ll.update!(data)
-      end
-    end
+    # JSON.parse(
+    #   Net::HTTP.get(URI('http://aidah-manaar.herokuapp.com/export/lessons.json'))
+    # ).each do |data|
+    #   ll = Lesson.find_by(id: data['id'])
+    #   if !ll
+    #     l = Lesson.new(data)
+    #     l.save!(validate: false)
+    #   else
+    #     ll.update!(data)
+    #   end
+    # end
 
-    JSON.parse(
-      Net::HTTP.get(URI('http://aidah-manaar.herokuapp.com/export/lesson_students.json'))
-    ).each do |data|
-      lsls = LessonStudent.find_by(id: data['id'])
-      if !lsls
-        ls = LessonStudent.new(data)
-        ls.save!(validate: false)
-      else
-        lsls.update!(data)
-      end
-    end
+    # JSON.parse(
+    #   Net::HTTP.get(URI('http://aidah-manaar.herokuapp.com/export/lesson_students.json'))
+    # ).each do |data|
+    #   lsls = LessonStudent.find_by(id: data['id'])
+    #   if !lsls
+    #     ls = LessonStudent.new(data)
+    #     ls.save!(validate: false)
+    #   else
+    #     lsls.update!(data)
+    #   end
+    # end
   end
 end
