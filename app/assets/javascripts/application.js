a4cea996989d085bf185e6e3948c81d5bb6cee6e
Guayo Mena <guayo.mena@gmail.com>
2016-11-19 13:37:39 -0600

    Started adding assets for template

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+
 //= require bootstrap/affix
 //= require bootstrap/alert
 //= require bootstrap/carousel
@@ -21,11 +22,34 @@
 //= require bootstrap/transition
 //= require bootstrap/tooltip
 //= require bootstrap-select
+//= bootstrap-hover-dropdown.min
+
 //= require jquery.maskedinput
-//= require video
 //= require jquery-ui
-//= require forms
-//= require header
+//= jquery.magnific-popup.min
+//= jquery.particleground
+//= jquery.sticky
+//= jquery.imagesloaded.min
+//= jquery-migrate-1.2.1.min
+//= jquery.backstretch.min
+//= jquery.easing.1.3
+//= jquery-migrate.min
+//= jquery.flexslider-min
+
+//= contact-form
+//= pricing
+
+//= template
+
+//= cube-portfolio
+//= masterslider-custom
+//= moderniz.min
+//= pace.min
+//= parallax.min
+//= tweetie.min
+//= waypoints.min
+//= wow.min
+//= revolution-custom
 
 $(function() {
   $('form').submit(function() {
