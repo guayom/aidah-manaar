134650f1b571dab138dd198aded3d71d61c42c82
Guayo Mena <guayo.mena@gmail.com>
2016-12-03 14:50:55 -0600

    finally fixed problem with master slider
    I changed the loading order of the javascripts
    Stopped loading many scripts not used in the template
    Moved vitaly's js to template.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,4 @@
-//= require jquery
-//= require jquery_ujs
-//= require jquery.maskedinput
-//= require jquery-ui
+//= require jquery.min
 //= require jquery-migrate.min
 //= require modernizr.min
 //= require jquery.easing.1.3
@@ -21,51 +18,32 @@
 // require tweetie.min
 //= require waypoints.min
 //= require jquery.sticky
+//= bootstrap-hover-dropdown.min
 //= require wow.min
+
+//= require template
+
 //= require contact-form
 
-//= require jquery.themepunch.enablelog.js
 //= require jquery.themepunch.tools.min.js
 //= require jquery.themepunch.revolution.min
+//  require jquery.themepunch.enablelog.js
 //= require revolution-custom
 
 // require jquery.cubeportfolio.min
 // require cube-portfolio
 // require pricing
 
-// require jquery.magnific-popup.min
-// require jquery.particleground
-
-//= require jquery.imagesloaded.min
-// require jquery.backstretch.min
-
 //= require masterslider
 //= require masterslider-custom
 //= require pace.min
 
-//= bootstrap-hover-dropdown.min
+// require jquery_ujs
+//= require jquery.maskedinput
+// require jquery-ui
 
-// require template
+// require jquery.magnific-popup.min
+// require jquery.particleground
 
-
-$(function() {
-  $('form').submit(function() {
-    $('#student_id_number').val($('#student_id_number').mask());
-    $('#student_phone').val($('#student_phone').mask());
-  });
-
-  $('.selectpicker').selectpicker();
-  $('#student_id_number').mask('9-9999-9999');
-  $('#student_phone').mask('9999-9999');
-});
-
-$(document).on('pjax:complete', function() {
-  $('form').submit(function() {
-    $('#student_id_number').val($('#student_id_number').mask());
-    $('#student_phone').val($('#student_phone').mask());
-  });
-
-  $('.selectpicker').selectpicker();
-  $('#student_id_number').mask('9-9999-9999');
-  $('#student_phone').mask('9999-9999');
-});
+// require jquery.imagesloaded.min
+// require jquery.backstretch.min
