3f4b4ffed7a2268903c2674999116d7256c984a3
Guayo Mena <guayo.mena@gmail.com>
2016-12-04 10:16:53 -0600

    Fixed smart quotes in mailer configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,13 +50,13 @@ Rails.application.configure do
   }
 
   config.action_mailer.smtp_settings = {
-  address: “smtp.gmail.com”,
+  address: "smtp.gmail.com",
   port: 587,
-  domain: ENV[“GMAIL_DOMAIN”],
-  authentication: “plain”,
+  domain: ENV["GMAIL_DOMAIN"],
+  authentication: "plain",
   enable_starttls_auto: true,
-  user_name: ENV[“GMAIL_USERNAME”],
-  password: ENV[“GMAIL_PASSWORD”]
+  user_name: ENV["GMAIL_USERNAME"],
+  password: ENV["GMAIL_PASSWORD"]
   }
 
 
